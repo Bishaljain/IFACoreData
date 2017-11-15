0e4040ba6ce9daf2fe81d0e9c92a93b8c03c58cc
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2017-11-16 07:13:06 +1100

    Project added.

---
+++ IFACoreData/IFACoreData/IFACoreData.h
@@ -0,0 +1,19 @@
+//
+//  IFACoreData.h
+//  IFACoreData
+//
+//  Created by Marcelo Schroeder on 16/11/17.
+//  Copyright © 2017 Marcelo Schroeder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IFACoreData.
+FOUNDATION_EXPORT double IFACoreDataVersionNumber;
+
+//! Project version string for IFACoreData.
+FOUNDATION_EXPORT const unsigned char IFACoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFACoreData/PublicHeader.h>
+
+
