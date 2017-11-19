2e352d445dbf110b61836436cb172b8d2b3af830
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2017-11-20 07:41:52 +1100

    Progress with migrating CoreData related code from IFACoreUI.

--- IFACoreData/IFACoreData/classes/IFACoreData.h
+++ IFACoreData/IFACoreData/classes/IFACoreData.h
@@ -16,4 +16,16 @@ FOUNDATION_EXPORT const unsigned char IFACoreDataVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <IFACoreData/PublicHeader.h>
 
-
+#import <IFACoreData/IFAPersistenceManager.h>
+#import <IFACoreData/IFAPreferencesManager.h>
+#import <IFACoreData/IFAHelpManager.h>
+#import <IFACoreData/IFAEnumerationEntity.h>
+#import <IFACoreData/IFASystemEntity.h>
+#import <IFACoreData/IFACoreDataUtils.h>
+#import <IFACoreData/IFACoreDataConstants.h>
+#import <IFACoreData/IFAEntityConfig.h>
+#import <IFACoreData/NSObject+IFACoreData.h>
+#import <IFACoreData/NSManagedObject+IFACoreData.h>
+#import <IFACoreData/NSManagedObjectContext+IFACoreData.h>
+#import <IFACoreData/IFAPersistenceChangeDetector.h>
+#import <IFACoreData/IFAPersistentEntityChangeObserver.h>
